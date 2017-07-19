0301475aaa8b5b0b5e06e6f26a91e117c263d7f4
Louwrens Labuschagne <louwrenslabuschagne@Louwrenss-MacBook-Pro.local>
2017-07-19 10:43:02 +0200

    added code to main.py, see README on how to set up google client library and install gcloud CLI

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # googleFacePi
+
+This project is set up using https://cloud.google.com/vision/docs/face-tutorial\
+To learn more about gcloud command-line commands, see the gcloud Tool Guide @ https://cloud.google.com/sdk/gcloud.
+
+This program uses the python library 'argparse' to aid with command line argument management, documentation can be found @ https://docs.python.org/3.3/library/argparse.html
+
+
+## Instalation Requirements
+
+Install the Google Client Library
+run this a few times if you get any errors
+
+	pip install --upgrade google-cloud-vision 
+
+The following example shows how to use the client library. To run it on your local workstation you must first install the Google Cloud SDK from https://cloud.google.com/sdk/docs/. To check if your pc is 32bit or 64bit run (OSX or Linux)
+
+	uname -m
+
+authenticate by running the following command:
+
+	gcloud auth application-default login
+
+Install Python.
+API reference.
+
+Install pillow.
+	
+	pip install pillow
--- main.py
+++ main.py
@@ -1,1 +1,50 @@
-print("Hallo Git")
+
+# import argparse
+# parser = argparse.ArgumentParser()
+# parser.add_argument('--foo', help='foo help')
+# args = parser.parse_args()
+
+# def defaultActionPrint():
+# 	print("Invalid argument, to see usage python main.py -h")
+# In [133]: def act2():
+#      ...:     print('act2')
+#      ...:     
+# In [134]: parser=argparse.ArgumentParser()
+# In [135]: parser.add_argument('-a',action='store_const',default=act1,const=act2);
+
+
+import io
+import os
+
+# Imports the Google Cloud client library
+from google.cloud import vision
+
+def getLabels():
+	# Instantiates a client
+	vision_client = vision.Client()
+
+	# The name of the image file to annotate
+	file_name = os.path.join(os.path.dirname(__file__),'resources/car_me_and_a_naartjie.jpg')
+
+	# Loads the image into memory
+	with io.open(file_name, 'rb') as image_file:
+	    image = vision_client.image(content=image_file.read())
+
+	# Performs label detection on the image file
+	labels = image.detect_labels()
+
+	return labels
+
+
+
+def main():
+	print("getting labels")
+	labels = getLabels()
+
+	print('Labels:')
+	for label in labels:
+	    print(label.description)
+
+#calling main() function on script excecution
+if __name__ == '__main__':
+    main()
